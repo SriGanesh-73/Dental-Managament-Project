194048798e5fef1e1863157f709d5d7fc12b7720
SriGanesh-73 <sriganeshthiyagarajan@gmail.com>
2025-03-17 17:47:52 +0000

    Appointment_booking_form.html

---
+++ Login_Register.js
@@ -0,0 +1,9 @@
+const userName = document.getElementById("username");
+const password = document.getElementById("password");
+
+const pattern1 = "/^[a-zA-Z][a-zA-Z0-9_]{2,15}$/";
+const pattern2 = "/^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*_])(?=.*\d).{8,}$/";
+
+userName.addEventListener("change",function (event){
+    
+});
