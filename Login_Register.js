3a3db02bb9e25d42e2801acb41517ad1902ece08
SriGanesh-73 <sriganeshthiyagarajan@gmail.com>
2025-03-18 12:51:34 +0530

    Update Login_Register.js

--- Login_Register.js
+++ Login_Register.js
@@ -1,9 +1,28 @@
 const userName = document.getElementById("username");
 const password = document.getElementById("password");
+const login_form = document.getElementById('login_form');
 
-const pattern1 = "/^[a-zA-Z][a-zA-Z0-9_]{2,15}$/";
-const pattern2 = "/^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*_])(?=.*\d).{8,}$/";
-
-userName.addEventListener("change",function (event){
+userName.addEventListener("change",function (userName,password){
+    let pattern1 = /^[a-zA-Z][a-zA-Z0-9_]{2,15}$/;
+    let pattern2 = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*_])(?=.*\d).{8,}$/;
     
+    const span = document.getElementById('username_err');
+    if(!pattern1.test(userName)){
+        span.textContent = "Please Enter a valid username";
+    }
+    if(!pattern2.test(password)){
+        span.textContent = "Please Enter a valid password";
+    }
+});
+login_form.addEventListener("submit",function (userName,password){
+    let pattern1 = /^[a-zA-Z][a-zA-Z0-9_]{2,15}$/;
+    let pattern2 = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*_])(?=.*\d).{8,}$/;
+    
+    const span = document.getElementById('username_err');
+    if(!pattern1.test(userName)){
+        span.textContent = "Please Enter a valid username";
+    }
+    if(!pattern2.test(password)){
+        span.textContent = "Please Enter a valid password";
+    }
 });
